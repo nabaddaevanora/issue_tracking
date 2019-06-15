9302ada51d0cd802be438ad6250cf87dab9b80ee
nabaddaevanora <nabaddaevanora@gmail.com>
2019-06-15 19:47:31 +0300

    add home page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "body-wrapper"> 
+        <div class = "header">
+          <div class = "header-logo">
+            <h1>Administration</h1>
+            </div>
+          <div class = "header-list" >
+              <ul>
+                <li><a href="#">Edit</a></li>
+                <li><a href="#">Comment</a></li>
+                <li><a href="#">Assign</a></li>
+                <li><a href="#">Cancel</a></li>
+              </ul>
+          </div>
+      </div>
+      <table align= "center">
+        <tr>
+          <td align="right">Tracker:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Subject:</td>
+          <td><input type = "text" ></input></td>
+        </tr>
+        <tr>
+          <td align="right">Description:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Status:</td>
+          <td>
+            <select name = "status">
+                <option value = "Active">Active</option>
+                <option value = "Closed">Closed</option>
+                <option value = "Resolved">Resolved</option>
+            </select>
+        </td>
+        </tr>
+        <tr>
+          <td align="right">Priority:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Assign:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Category:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Company name:</td>
+          <td><input type = "text"></input></td>
+        </tr>
+        <tr>
+          <td align="right">Contact:</td>
+        <td><input type = "text"></input></td>
+        </tr>
+        <br></br>
+          <div align="right">LOG TIME</div>
+          <tr>
+            <td align="right">Spent time:</td>
+            <td><input type = "text"></input></td>hours
+            </tr>
+          <br></br>
+          <tr>
+            <td align="right">Activity:</td>
+            <td><input type = "text"></input></td>
+            </tr>
+          <br></br>
+         
+      </table>
+      <p align ="center"> Comment:</p>
+      <p><textarea className="text-area" id = "msg" name = "user_message"></textarea></p>
     </div>
+  </div>
   );
 }
 
